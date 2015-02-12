63fa72af9c21c02d9f933cef8169c65ddd4851d2
Lauren McCarthy <laurmccarthy@gmail.com>
2015-02-12 00:56:38 -0500

    socketio

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
+
 var express = require('express');
 var app = express();
+app.set('port', process.env.PORT || 3000);
 
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+server.listen(app.get('port'));
+
+var socket;
 var hr = 0;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+
+io.on('connection', function (socket) {
+  socket = socket;
+  console.log('socket connected');
+});
+
+// app.get('/', function (req, res) {
+//   res.sendfile(__dirname + '/public/index.html');
+// })
+
+//app.use(express.static('public'));
 
 
 app.get('/update_hr', function (req, res) {
   console.log(req.query);
   hr = parseInt(req.query.hr, 10);
   console.log(hr);
+  if (socket) socket.broadcast.emit('hr', hr);
   res.send('thanks');
 })
 
@@ -19,8 +35,6 @@ app.get('/update_hr', function (req, res) {
 app.use(express.static(__dirname + '/public'));
 
 
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function () {
-  console.log('Example app listening at http://%s:%s', server.address().address, server.address().port);
-});
+// var server = app.listen(app.get('port'), function () {
+//   console.log('Example app listening at http://%s:%s', server.address().address, server.address().port);
+// });
