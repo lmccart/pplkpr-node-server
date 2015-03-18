f0463520a2b0e3c6148c5e1fbf31dbbe91146f20
Lauren McCarthy <laurmccarthy@gmail.com>
2015-03-18 19:20:18 +0000

    server

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ console.log('Listening on '+app.get('port'));
 // CONFIG MONGO
 var MongoClient = require('mongodb').MongoClient
 var assert = require('assert');
-var reports;
+var reports, people;
 
 MongoClient.connect(process.env.MONGOLAB_URI, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to mongodb");
   reports = db.collection("reports");
+  people = db.collection("people");
 });
 
 // CONFIG MULTER
@@ -37,26 +38,39 @@ app.use(multer({ dest: './uploads/',
 
 app.get('/add_report', function (req, res) {
   console.log(req.query);
-  var r = {
-    name: req.query.name,
-    number: req.query.number,
-    emotion: req.query.emotion,
-    value: parseFloat(req.query.value),
-    timestamp: new Date().getTime()
-  }
 
-  // Insert some documents
-  reports.insert(r, function(err, result) {
-    assert.equal(err, null);
-    console.log("inserted");
-    //callback(result);
+  get_person(req.query.name, req.query.number, function(id) {   
+
+    var r = {
+      person: id,
+      emotion: req.query.emotion,
+      value: parseFloat(req.query.value),
+      timestamp: new Date().getTime()
+    }
+
+    reports.insert(r, function(err, result) {
+      assert.equal(err, null);
+      console.log('inserted report');
+      //callback(result);
+    });
+   
   });
-
   res.send('thanks');
 })
 
 app.post('/add_person',function(req,res){
   console.log(req.body.name);
+  var p = {
+    name: req.body.name,
+    number: req.body.number,
+    photo: req.body.filename
+  }
+
+  people.insert(p, function(err, result) {
+    assert.equal(err, null);
+    console.log('inserted person');
+    //callback(result);
+  });
   res.send('thanks');
 });
 
@@ -64,3 +78,14 @@ app.get('/get_emotion', function (req, res) {
   var e = req.query.emotion;
   res.send({name: n, pic: p}); 
 });
+
+function get_person(name, num, cb) {
+  // find person or kill
+  var id = 0; // pend
+  cb(id);
+}
+
+function eval_people() {
+  // for each emotion find maxs, store somehow
+}
+
